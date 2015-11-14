6a8a106b6444b40eb316f3d41f97b763016d9b64
RhettDelFierro <thinquetank@gmail.com>
2015-11-13 16:19:58 -0800

    almoooooost done with semi-oop

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ var player_1 = $("<div class='player_1'><h2>Kiddo</h2></div>"),
     player_2 = $("<div class='player_2'><h2>O-Ren Ishii</h2></div>"),
     game_container1 = $("<div class='game_container1'></div>"),
     row_1 = $("<div class='row row_1'></div>"),
-    box_1 = $("<div class='box one col1 dia1'></div>"),
-    box_2 = $("<div class='box two col2'></div>"),
-    box_3 = $("<div class='box three col3 dia2'></div>"),
+    box_1 = $("<div class='box one col1 dia1' id='box_1'></div>"),
+    box_2 = $("<div class='box two col2' id='box_2'></div>"),
+    box_3 = $("<div class='box three col3 dia2' id='box_3'></div>"),
     row_2 = $("<div class='row row_2'></div>"),
-    box_4 = $("<div class='box four col1'></div>"),
-    box_5 = $("<div class='box five col2 dia1 dia2'></div>"),
-    box_6 = $("<div class='box six col3'></div>"),
+    box_4 = $("<div class='box four col1' id='box_4'></div>"),
+    box_5 = $("<div class='box five col2 dia1 dia2' id='box_5'></div>"),
+    box_6 = $("<div class='box six col3' id='box_6'></div>"),
     row_3 = $("<div class='row row_3'></div>"),
-    box_7 = $("<div class='box seven col1 dia2'></div>"),
-    box_8 = $("<div class='box eight col2'></div>"),
-    box_9 = $("<div class='box nine col3 dia1'></div>"),
+    box_7 = $("<div class='box seven col1 dia2' id='box_7'></div>"),
+    box_8 = $("<div class='box eight col2' id='box_8'></div>"),
+    box_9 = $("<div class='box nine col3 dia1' id='box_9'></div>"),
     game_container2 = $("<div class='game_container2'></div>"),
     b2_row1 = $("<div class='b2_row b2_row1'></div>"),
     b2_box1 = $("<div class='b2_box b2_box1 b2_col1 b2_dia1'></div>"),
@@ -98,13 +98,36 @@ function boardReset() {
     d2 = [];
 }
 //***********************************************OOP Board*******************************************//
-//var board = {
-//        this.number_of_cards = 9, //count down for tie.
-//        this.
-//.
-//turn_indicator = null; //if player one clicks, that has a property that can be sent to the board.
-//
-//}
+var board = {};
+board.number_of_cards = size; //count down for tie.
+board.cards_do_your_thing = function (clickedVariable, playerTurn) {
+    console.log('called :', clickedVariable, playerTurn);
+    var cards = {
+        row1: ["box_1", "box_2", "box_3"],
+        row2: ["box_4", "box_5", "box_6"],
+        row3: ["box_7", "box_8", "box_9"],
+        col1: ["box_1", "box_4", "box_7"],
+        col2: ["box_2", "box_5", "box_8"],
+        col3: ["box_3", "box_6", "box_9"],
+        diag1: ["box_1", "box_5", "box_9"],
+        diag2: ["box_3", "box_5", "box_7"]
+    };
+    for(var x in cards){
+        console.log(x);
+        for (i=0; i < x.length; i++){
+            if (clickedVariable === x[i]){
+                console.log('found a match!');
+                //playerTurn.x.text++;
+                //var concatenate = x.text;
+                console.log(x[i]);
+                playerTurn.x++;
+                win_condition_check();
+            }
+        }
+    }
+};
+//if player one clicks, that has a property that can be sent to the board.
+
 
 //***********************************************WIN CONDITION***************************************//
 function win_condition_check() {
@@ -148,68 +171,68 @@ function Player(player_number) {
     this.b2_diag2 = 0;
 }
 
-        //this.row1_add = function () {
-        //    this.row1++;
-        //};
-        //this.row2_add = function () {
-        //    this.row2++;
-        //};
-        //this.row3_add = function () {
-        //    this.row3++;
-        //};
-        //this.col1_add = function () {
-        //    this.col1++;
-        //};
-        //this.col2_add = function () {
-        //    this.col2++;
-        //};
-        //this.col3_add = function () {
-        //    this.col3++;
-        //};
-        //this.diag1_add = function () {
-        //    this.diag1++;
-        //};
-        //this.diag2_add = function () {
-        //    this.diag2++;
-        //};
-        //
-        //
-        //this.b2_row1_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_row2_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_row3_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_row4_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_row5_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_col1_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_col2_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_col3_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_col4_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_col5_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_dia1_add = function () {
-        //    this.row1++;
-        //};
-        //this.b2_dia2_add = function () {
-        //    this.row1++;
-        //};
+//this.row1_add = function () {
+//    this.row1++;
+//};
+//this.row2_add = function () {
+//    this.row2++;
+//};
+//this.row3_add = function () {
+//    this.row3++;
+//};
+//this.col1_add = function () {
+//    this.col1++;
+//};
+//this.col2_add = function () {
+//    this.col2++;
+//};
+//this.col3_add = function () {
+//    this.col3++;
+//};
+//this.diag1_add = function () {
+//    this.diag1++;
+//};
+//this.diag2_add = function () {
+//    this.diag2++;
+//};
+//
+//
+//this.b2_row1_add = function () {
+//    this.row1++;
+//};
+//this.b2_row2_add = function () {
+//    this.row1++;
+//};
+//this.b2_row3_add = function () {
+//    this.row1++;
+//};
+//this.b2_row4_add = function () {
+//    this.row1++;
+//};
+//this.b2_row5_add = function () {
+//    this.row1++;
+//};
+//this.b2_col1_add = function () {
+//    this.row1++;
+//};
+//this.b2_col2_add = function () {
+//    this.row1++;
+//};
+//this.b2_col3_add = function () {
+//    this.row1++;
+//};
+//this.b2_col4_add = function () {
+//    this.row1++;
+//};
+//this.b2_col5_add = function () {
+//    this.row1++;
+//};
+//this.b2_dia1_add = function () {
+//    this.row1++;
+//};
+//this.b2_dia2_add = function () {
+//    this.row1++;
+//};
 
 
 //===============document ready function start==============//
@@ -230,6 +253,27 @@ $(document).ready(function () {
     $(game_container1).append(row_1, row_2, row_3);
     $(game_container2).append(b2_row1, b2_row2, b2_row3, b2_row4, b2_row5);
     $("body").append(title, reboot, player_1, board_options, player_2);
+
+//************************************************OOP BOARD CLICK CALL**********************************//
+//    $('.box').on('click', function () {
+//        var variableFromCardClass = this.attr("id");
+//        if (swap % 2 === 0) {
+//            board.cards_do_your_thing(variableFromCardClass, player1);
+//        }
+//        else {
+//            board.cards_do_your_thing(variableFromCardClass, player2);
+//        }
+//    });
+//
+//    $('.b2_box').on('click', function () {
+//        var variableFromCardClass = this.attr("id");
+//        if (swap % 2 === 0) {
+//            board.cards_do_your_thing(variableFromCardClass, player1);
+//        }
+//        else {
+//            board.cards_do_your_thing(variableFromCardClass, player2);
+//        }
+//    });
 
 //=================3x3 Board Creation==================//
 
@@ -265,6 +309,7 @@ $(document).ready(function () {
 //=================Image Append 3x3 board==================//
     $(document).on('click', '.box', function () {
         $(this).addClass('marked');
+        var variableFromCardClass = $(this).attr("id");
         console.log('hi');
         if (swap % 2 === 0) {
             //player 1 gets to go
@@ -273,12 +318,15 @@ $(document).ready(function () {
             sword.play();
             $(this).addClass("x").removeClass("black_background").addClass("yellow_background");
             $(this).attr('marked', 'true');
+            board.cards_do_your_thing(variableFromCardClass, player1);
+
         } else {
             //player 2 gets to go
             var y = $('<img>').attr('src', 'images/meteor_hammer.png');
             meteor_hammer.play();
             //$(this).append(y);
             $(this).addClass("o");
+            board.cards_do_your_thing(variableFromCardClass, player2);
         }
         increment();
     });
@@ -310,197 +358,197 @@ $(document).ready(function () {
 
 //=================3x3 win condition=====================//
 
-
-    $(row_1).on('click', '.box', function () {
-        if (swap % 2 === 0) {
-            player1.row1++;
-        }
-        else {
-            player2.row1++;
-        }
-        win_condition_check();
-    });
-
-    $(row_2).on('click', '.box', function () {
-        if (swap % 2 === 0) {
-            player1.row2++;
-        }
-        else {
-            player2.row2++;
-        }
-        win_condition_check();
-    });
-
-    $(row_3).on('click', '.box', function () {
-        if (swap % 2 === 0) {
-            player1.row3++;
-        }
-        else {
-            player2.row3++;
-        }
-        win_condition_check();
-    });
-
-    $(document).on('click', '.col1', function () {
-        if (swap % 2 === 0) {
-            player1.col1++;
-        }
-        else {
-            player2.col1++;
-        }
-        win_condition_check();
-    });
-
-    $(document).on('click', '.col2', function () {
-        if (swap % 2 === 0) {
-            player1.col2++;
-        }
-        else {
-            player2.col2++;
-        }
-        win_condition_check();
-    });
-
-    $(document).on('click', '.col3', function () {
-        if (swap % 2 === 0) {
-            player1.col3++;
-        }
-        else {
-            player2.col3++;
-        }
-        win_condition_check();
-    });
-
-    $(document).on('click', '.dia1', function () {
-        if (swap % 2 === 0) {
-            player1.diag1++;
-        }
-        else {
-            player2.diag1++;
-        }
-        win_condition_check();
-    });
-
-    $(document).on('click', '.dia2', function () {
-        if (swap % 2 === 0) {
-            player1.diag2++;
-        }
-        else {
-            player2.diag2++;
-        }
-        win_condition_check();
-    });
+    //
+    //$(row_1).on('click', '.box', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.row1++;
+    //    }
+    //    else {
+    //        player2.row1++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(row_2).on('click', '.box', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.row2++;
+    //    }
+    //    else {
+    //        player2.row2++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(row_3).on('click', '.box', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.row3++;
+    //    }
+    //    else {
+    //        player2.row3++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(document).on('click', '.col1', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.col1++;
+    //    }
+    //    else {
+    //        player2.col1++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(document).on('click', '.col2', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.col2++;
+    //    }
+    //    else {
+    //        player2.col2++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(document).on('click', '.col3', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.col3++;
+    //    }
+    //    else {
+    //        player2.col3++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(document).on('click', '.dia1', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.diag1++;
+    //    }
+    //    else {
+    //        player2.diag1++;
+    //    }
+    //    win_condition_check();
+    //});
+    //
+    //$(document).on('click', '.dia2', function () {
+    //    if (swap % 2 === 0) {
+    //        player1.diag2++;
+    //    }
+    //    else {
+    //        player2.diag2++;
+    //    }
+    //    win_condition_check();
+    //});
 
 //=========5x5 win condition =============//
-
-    $(b2_row1).on('click', '.b2_box', function () {
-        if (swap % 2 === 0) {
-            player1.b2_row1++;
-        }
-        else {
-            player2.b2_row1++;
-        }
-        win_condition_check();
-    });
-    $(b2_row2).on('click', '.b2_box', function () {
-        if (swap % 2 === 0) {
-            player1.b2_row2++;
-        }
-        else {
-            player2.b2_row2++;
-        }
-        win_condition_check();
-    });
-    $(b2_row3).on('click', '.b2_box', function () {
-        if (swap % 2 === 0) {
-            player1.b2_row3++;
-        }
-        else {
-            player2.b2_row3++;
-        }
-        win_condition_check();
-    });
-    $(b2_row4).on('click', '.b2_box', function () {
-        if (swap % 2 === 0) {
-            player1.b2_row4++;
-        }
-        else {
-            player2.b2_row4++;
-        }
-        win_condition_check();
-    });
-    $(b2_row5).on('click', '.b2_box', function () {
-        if (swap % 2 === 0) {
-            player1.b2_row5++;
-        }
-        else {
-            player2.b2_row5++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_col1', function () {
-        if (swap % 2 === 0) {
-            player1.b2_col1++;
-        }
-        else {
-            player2.b2_col1++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_col2', function () {
-        if (swap % 2 === 0) {
-            player1.b2_col2++;
-        }
-        else {
-            player2.b2_col2++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_col3', function () {
-        if (swap % 2 === 0) {
-            player1.b2_col3++;
-        }
-        else {
-            player2.b2_col3++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_col4', function () {
-        if (swap % 2 === 0) {
-            player1.b2_col4++;
-        }
-        else {
-            player2.b2_col4++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_col5', function () {
-        if (swap % 2 === 0) {
-            player1.b2_col5++;
-        }
-        else {
-            player2.b2_col5++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_dia1', function () {
-        if (swap % 2 === 0) {
-            player1.b2_diag1++;
-        }
-        else {
-            player2.b2_diag1++;
-        }
-        win_condition_check();
-    });
-    $(document).on('click', '.b2_dia2', function () {
-        if (swap % 2 === 0) {
-            player1.b2_diag2++;
-        }
-        else {
-            player2.b2_diag2++;
-        }
-        win_condition_check();
-    });
+//
+//    $(b2_row1).on('click', '.b2_box', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_row1++;
+//        }
+//        else {
+//            player2.b2_row1++;
+//        }
+//        win_condition_check();
+//    });
+//    $(b2_row2).on('click', '.b2_box', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_row2++;
+//        }
+//        else {
+//            player2.b2_row2++;
+//        }
+//        win_condition_check();
+//    });
+//    $(b2_row3).on('click', '.b2_box', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_row3++;
+//        }
+//        else {
+//            player2.b2_row3++;
+//        }
+//        win_condition_check();
+//    });
+//    $(b2_row4).on('click', '.b2_box', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_row4++;
+//        }
+//        else {
+//            player2.b2_row4++;
+//        }
+//        win_condition_check();
+//    });
+//    $(b2_row5).on('click', '.b2_box', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_row5++;
+//        }
+//        else {
+//            player2.b2_row5++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_col1', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_col1++;
+//        }
+//        else {
+//            player2.b2_col1++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_col2', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_col2++;
+//        }
+//        else {
+//            player2.b2_col2++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_col3', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_col3++;
+//        }
+//        else {
+//            player2.b2_col3++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_col4', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_col4++;
+//        }
+//        else {
+//            player2.b2_col4++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_col5', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_col5++;
+//        }
+//        else {
+//            player2.b2_col5++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_dia1', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_diag1++;
+//        }
+//        else {
+//            player2.b2_diag1++;
+//        }
+//        win_condition_check();
+//    });
+//    $(document).on('click', '.b2_dia2', function () {
+//        if (swap % 2 === 0) {
+//            player1.b2_diag2++;
+//        }
+//        else {
+//            player2.b2_diag2++;
+//        }
+//        win_condition_check();
+//    });
 
 //========= Reset ==============//
 
